f1a329054fbd98b2134e48947e3bf191ac4a7458
Andre Pena <andrerpena@gmail.com>
2025-12-20 21:21:13 +0000

    Fix layout switch while editing strings

--- packages/frontend/src/components/form/renderers/string/StringViewFieldRenderer.tsx
+++ packages/frontend/src/components/form/renderers/string/StringViewFieldRenderer.tsx
@@ -5,7 +5,7 @@ export const StringViewFieldRenderer = ({
 }: ViewFieldRendererProps) => {
   const value = fieldState.state.value || "";
   return (
-    <div className="flex w-full min-w-0 px-3 py-1 rounded-md border border-transparent text-base text-foreground">
+    <div className="flex items-center h-9 w-full min-w-0 px-3 py-1 rounded-md border border-transparent text-base text-foreground">
       {value || <span className="text-muted-foreground italic">Empty</span>}
     </div>
   );
