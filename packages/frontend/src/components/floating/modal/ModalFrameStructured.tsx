056bba99925c6d295c668a2b0414abe47410925b
Andre Pena <andrerpena@gmail.com>
2026-06-28 12:16:28 +0100

    Refactor header and body padding in Drawer and Modal components

--- packages/frontend/src/components/floating/modal/ModalFrameStructured.tsx
+++ packages/frontend/src/components/floating/modal/ModalFrameStructured.tsx
@@ -19,15 +19,9 @@ export const ModalFrameStructured: FC<ModalFrameStructuredProps> = ({
       data-component="ModalFrameStructured"
       className={`rounded-lg bg-card border shadow-sm max-h-[calc(100vh_-_2rem)] flex flex-col overflow-hidden ${className}`}
     >
-      {header && <div className="shrink-0 p-4 pb-3">{header}</div>}
+      {header && <div className="shrink-0 px-4 pt-4">{header}</div>}
 
-      {body && (
-        <div
-          className={`flex-1 overflow-y-auto min-h-0 ${header ? "px-4 pb-4" : "p-4"}`}
-        >
-          {body}
-        </div>
-      )}
+      {body && <div className="flex-1 overflow-y-auto min-h-0 p-4">{body}</div>}
 
       {footer && <div className="shrink-0 border-t">{footer}</div>}
     </div>
